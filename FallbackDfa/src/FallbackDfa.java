27ca8d655b127b0c34e8f22d33a97524e9b4a40c
Shehab Solyman <Shehabamgad134@gmail.com>
2023-03-13 21:48:57 +0200

    Update FallbackDfa.java

--- FallbackDfa/src/FallbackDfa.java
+++ FallbackDfa/src/FallbackDfa.java
@@ -1,34 +1,109 @@
 package csen1002.main.task3;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Stack;
+
 /**
  * Write your info here
- * 
- * @name Jane Smith
- * @id 46-0234
- * @labNumber 07
+ *
+ * @name Shehabeldin Solyman
+ * @id 46-2664
+ * @labNumber 12
  */
 
 public class FallbackDfa {
 
-	/**
-	 * Constructs a Fallback DFA
-	 * 
-	 * @param fdfa A formatted string representation of the Fallback DFA. The string
-	 *             representation follows the one in the task description
-	 */
-	public FallbackDfa(String fdfa) {
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @param input The string to simulate by the FDFA.
-	 * 
-	 * @return Returns a formatted string representation of the list of tokens. The
-	 *         string representation follows the one in the task description
-	 */
-	public String run(String input) {
-		// TODO Auto-generated method stub
-		return null;
+	int armR;
+	int armL;
+	
+	int startState;
+	int currentState;
+	String[] transitions;
+	ArrayList<Integer> acceptStates;
+	
+	static Stack<Integer> readings;
+	
+	String result;
+	
+	public void read(String input) {
+		while(armL<input.length()) {
+			for(String transition : transitions) {
+				String[] arrayT = transition.split(",");
+				if(Integer.parseInt(arrayT[0])==currentState && arrayT[1].equals(input.charAt(armL)+"")) {
+					currentState = Integer.parseInt(arrayT[2]);
+					armL++;
+					readings.push(currentState);
+					break;
+				}
+			}
+		}
 	}
 	
+	public void backTrack(String input) {
+		boolean foundAcceptState = false;
+		Stack<Integer> cloneStack = new Stack<>();
+		cloneStack.addAll(readings);
+		int cloneL = armL;
+		while(armL>=armR) {
+			if(acceptStates.contains(currentState)){
+				foundAcceptState = true;
+				result += input.substring(armR, armL+1)+","+currentState+";";
+				armR = ++armL;
+				currentState = startState;
+				readings.clear();
+				break;
+			}
+			else {
+				currentState = readings.pop();
+				armL--;
+			}
+		}
+		if(foundAcceptState) {
+			runHelper(input);
+		}
+		else {
+			result += input.substring(armR)+","+cloneStack.pop()+";";
+		}
+	}
+	
+	
+	public FallbackDfa(String fdfa) {
+		String[] input = fdfa.split("#");
+		
+		startState = Integer.parseInt(input[3]);
+		currentState = Integer.parseInt(input[3]);
+		
+		acceptStates = new ArrayList<Integer>();
+		for(String state : input[4].split(";")) {
+			acceptStates.add(Integer.parseInt(state));
+		}
+		
+		transitions = input[2].split(";");
+	}
+
+	public String run(String input) {
+		armR = 0;
+		armL = 0;
+		readings = new Stack<Integer>();
+		result = "";
+		
+		runHelper(input);
+		
+		return result.substring(0, result.length()-1);
+	}
+	
+	public void runHelper(String input) {
+
+		readings.add(currentState);
+		read(input);
+		
+		if(acceptStates.contains(currentState)) {
+			result += input.substring(armR)+","+currentState+";";
+		}
+		else {
+			backTrack(input);
+		}
+		
+	}
 }
